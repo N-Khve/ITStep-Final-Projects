229f248e840d837cf7f3540f139a8f0c1a5bba62
idk-something <nikan7304@gmail.com>
2026-04-22 22:16:01 +0400

    Added Translator Project

---
+++ C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("ტესტ");
+            Console.WriteLine("еуые");
+        }
+    }
+}
